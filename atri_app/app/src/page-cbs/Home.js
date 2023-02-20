850753f5e53770c8a19b124ebabecc24197a726a
Harry <abhishekchauhan664@gmail.com>
2023-02-20 16:48:00 +0530

    initial comit

---
+++ atri_app/app/src/page-cbs/Home.js
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+import { callbackFactory } from "../utils/callbackFactory";
+export function useTextBox1Cb() {
+	const onClick = useCallback(callbackFactory("TextBox1", "Home", "/", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useButton5Cb() {
+	const onClick = useCallback(callbackFactory("Button5", "Home", "/", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
---
+++ atri_app/app/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useLayoutEffect, useEffect } from "react";
+import useStore, { updateStoreStateFromController } from "../hooks/useStore";
+import useIoStore from "../hooks/useIoStore";
+import { useNavigate, useLocation } from "react-router-dom";
+import { subscribeInternalNavigation } from "../utils/navigate";
+import {fetchPageProps} from "../utils/fetchPageProps"
+import { TextBox } from "@atrilabs/react-component-manifests/src/manifests/TextBox/TextBox.tsx";
+import { Button } from "@atrilabs/react-component-manifests/src/manifests/Button/Button.tsx";
+import { useTextBox1Cb, useButton5Cb } from "../page-cbs/Home";
+import "../page-css/Home.css";
+import "../custom/Home";
+
+export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsub = subscribeInternalNavigation((url) => {
+      navigate(url);
+    });
+    return unsub;
+  }, [navigate]);
+
+  const location = useLocation();
+  useLayoutEffect(()=>{
+    fetchPageProps(location.pathname, location.search).then((res)=>{
+      updateStoreStateFromController(res.pageName, res.pageState)
+    })
+  }, [location])
+
+  const TextBox1Props = useStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1IoProps = useIoStore((state)=>state["Home"]["TextBox1"]);
+const TextBox1Cb = useTextBox1Cb()
+const Button5Props = useStore((state)=>state["Home"]["Button5"]);
+const Button5IoProps = useIoStore((state)=>state["Home"]["Button5"]);
+const Button5Cb = useButton5Cb()
+
+  return (<>
+  <TextBox className="p-Home TextBox1 bpt" {...TextBox1Props} {...TextBox1Cb} {...TextBox1IoProps}/>
+<Button className="p-Home Button5 bpt" {...Button5Props} {...Button5Cb} {...Button5IoProps}/>
+  </>);
+}
